1785c54c351506feccf3acca80a261df9c551e3c
vishalsingh17 <vishal170997@gmail.com>
2023-04-12 18:30:23 +0530

    Modified setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-__version__ = "0.0.1"
+__version__ = "0.0.3"
 
 REPO_NAME = "PythonPackage"
 AUTHOR_USERNAME = "VS17"
@@ -12,16 +12,17 @@ AUTHOR_EMAIL = "vishal170997@gmail.com"
 GITHUB_USERNAME = "vishalsingh17"
 
 setuptools.setup(
-    name = SRC_REPO,
-    version = __version__,
-    author = AUTHOR_USERNAME,
-    author_email = AUTHOR_EMAIL,
-    description = "A python package",
-    long_description_content = "text/markdown",
-    url = f"https://github.com/{GITHUB_USERNAME}/{REPO_NAME}",
-    projects_urls = {
-        "Bug Tracker": f"https://github.com/{GITHUB_USERNAME}/{REPO_NAME}/issues"
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USERNAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{GITHUB_USERNAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{GITHUB_USERNAME}/{REPO_NAME}/issues",
     },
-    package_dir = {"":"src"},
-    packages = setuptools.find_packages(where="src")
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
 )
