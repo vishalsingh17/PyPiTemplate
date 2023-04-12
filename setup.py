583135c34a5a240edd995cbd3218edabbfa4e8ed
vishalsingh17 <vishal170997@gmail.com>
2023-04-12 18:09:01 +0530

    Modified setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.1"
 
-REPO_NAME = "IPYNBrenderer"
+REPO_NAME = "PythonPackage"
 AUTHOR_USERNAME = "VS17"
-SRC_REPO = "IPYNBrenderer"
+SRC_REPO = "PythonPackage"
 AUTHOR_EMAIL = "vishal170997@gmail.com"
 GITHUB_USERNAME = "vishalsingh17"
 
